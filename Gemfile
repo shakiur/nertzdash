f686263360c17b2db111178e1053a76d05b55f5a
Sam Yau <samyau21@gmail.com>
2019-06-28 13:01:22 -0400

    add bootstrap 4.0.0 gem

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bundler'
+
+gem 'bootstrap', '~> 4.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/games' => 'games#index', as: :games
+  post '/games/create' => 'games#create', as: :create_game
 
   get '/teams' => 'teams#index', as: :teams
   post '/teams/create' => 'teams#create', as: :create_team
