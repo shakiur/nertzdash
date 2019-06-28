823bdc4378c837c37b40f188b8a348f28b8a1039
Shakiur Rahman <shakiur@gmail.com>
2019-06-28 12:57:49 -0400

    boiler plate routes/controller/view for teams

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,9 @@
+class TeamsController < ApplicationController
+  def index
+
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/teams' => 'teams#index', as: :teams
+  post '/teams/create' => 'teams#create', as: :create_team
 end
