113429798b02a96c32180961fd09a5ab0638c1e7
Shakiur Rahman <shakiur@gmail.com>
2019-06-29 15:51:51 -0400

    create a team, fix player2 id

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,27 @@
 class TeamsController < ApplicationController
   def index
-
+    @players = Player.all
   end
 
   def create
-    
+    team_name = params[:team_name]
+    player1_id = params[:player1]
+    player2_id = params[:player2]
+
+    team = Team.new
+    team.name = team_name
+    team.save
+
+    team_player1 = TeamPlayer.new
+    team_player1.team_id = team.id
+    team_player1.player_id = player1_id
+    team_player1.save
+
+    team_player2 = TeamPlayer.new
+    team_player2.team_id = team.id
+    team_player2.player_id = player2_id
+    team_player2.save
+
+    redirect_to teams_path
   end
 end
