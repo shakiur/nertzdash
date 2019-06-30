0623e5585c10f969f1a75ec6a0c18ff2a0ef3621
Sam Yau <samyau21@gmail.com>
2019-06-30 05:43:46 -0400

    finished new game form. dummy pretty past games grid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def index
     @hello = "world"
   end
+
+  def create
+    return redirect_to games_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => redirect('/games')
 
   get '/games' => 'games#index', as: :games
-  post '/games/create' => 'games#create', as: :create_game
+  put '/games/create' => 'games#create', as: :create_game
 
   get '/teams' => 'teams#index', as: :teams
   post '/teams/create' => 'teams#create', as: :create_team
