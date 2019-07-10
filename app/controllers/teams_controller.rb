0ae40f9239f6ad04b4cc7b251bba8e2d6b101ab0
Shakiur Rahman <shakiur@gmail.com>
2019-07-09 22:23:07 -0400

    handle different error states for singles and doubles teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,20 +1,15 @@
 class TeamsController < ApplicationController
   def index
     @all_players = Player.all
-    @all_teams = Team.includes(team_players: :player).all 
+    @all_teams = Team.includes(team_players: :player).all
   end
 
   def create
     team_name = params[:team_name]
+    team_type = params[:team_type]
+
     player1_id = params[:player1]
     player2_id = params[:player2]
-
-    player1 = Player.find(player1_id)
-    player2 = Player.find(player2_id)
-
-    player1_teams = player1.teams
-    player2_teams = player2.teams
-    overlapping_team = (player1_teams & player2_teams).first
 
     if team_name.blank?
       flash[:notice] = 'Team name cannot be blank'
@@ -26,19 +21,53 @@ class TeamsController < ApplicationController
       return redirect_to teams_path
     end
 
-    if player1_id == player2_id
-      flash[:notice] = "Player '#{player1.name}' cannot be on the same team twice"
-      return redirect_to teams_path
+    if team_type == 'double'
+      if player1_id.blank?
+        flash[:notice] = "Player 1 must be selected"
+        return redirect_to teams_path
+      end
+
+      if player2_id.blank?
+        flash[:notice] = "Player 2 must be selected"
+        return redirect_to teams_path
+      end
+
+      player1 = Player.find(player1_id)
+      player2 = Player.find(player2_id)
+
+      player1_teams = player1.teams
+      player2_teams = player2.teams
+      overlapping_team = (player1_teams & player2_teams).first
+
+      if player1_id == player2_id
+        flash[:notice] = "Player '#{player1.name}' cannot be on the same team"
+        return redirect_to teams_path
+      end
+
+      if overlapping_team.present?
+        flash[:notice] = "Players '#{player1.name}' and '#{player2.name}' already belong to team '#{overlapping_team.name}'"
+        return redirect_to teams_path
+      end
     end
 
-    if overlapping_team.present?
-      flash[:notice] = "Players '#{player1.name}' and '#{player2.name}' already belong to team '#{overlapping_team.name}'"
-      return redirect_to teams_path
+    if team_type == 'single'
+      if player1_id.blank?
+        flash[:notice] = 'Player 1 must be selected'
+        return redirect_to teams_path
+      end
+
+      player1 = Player.find(player1_id)
+
+      if player1.teams.where(team_type: 'single').any?
+        flash[:notice] = "Player '#{player1.name}' is already on a singles team"
+        return redirect_to teams_path
+      end
     end
 
     begin
       ActiveRecord::Base.transaction do
         team = Team.new
+        team.team_type = team_type
         team.name = team_name
         team.save!
 
@@ -47,13 +76,16 @@ class TeamsController < ApplicationController
         team_player1.player_id = player1_id
         team_player1.save
 
-        team_player2 = TeamPlayer.new
-        team_player2.team_id = team.id
-        team_player2.player_id = player2_id
-        team_player2.save
+        if team_type == 'double'
+          team_player2 = TeamPlayer.new
+          team_player2.team_id = team.id
+          team_player2.player_id = player2_id
+          team_player2.save
+        end
       end
     rescue => e
-      byebug
+      flash[:notice] = "There was an error creating your team"
+      return redirect_to teams_path
     end
 
     return redirect_to teams_path
