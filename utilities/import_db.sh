ce0a04b9f6269df0aecdfe2255e2d34beb039637
Sam Yau <samyau21@gmail.com>
2019-07-27 15:29:43 -0400

    add utils dir. add import_db script for prod db dump fetch import

---
+++ utilities/import_db.sh
@@ -0,0 +1,2 @@
+# Import production DB to local dev
+mysqldump -u lnneln32kgmk4q6b -h k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com --password="is351rzc0rrkvvjj" --set-gtid-purged=OFF rdm46h728v1t96ff | mysql -u root nertzdash_development
