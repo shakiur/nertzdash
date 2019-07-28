13e81adeaa1c3ff24d956df56e263fb7bce4c309
Sam Yau <samyau21@gmail.com>
2019-07-28 18:24:55 -0400

    average score doubles

---
+++ lib/stats/average_score_doubles.rb
@@ -0,0 +1,30 @@
+module AverageScoreDoubles
+ module_function
+
+  def description
+    "Average score of doubles teams per round. Does not account for teams wth less than five rounds on record."
+  end
+
+  def graph_type
+    :column_chart
+  end
+
+  def graph_options
+    {
+      xtitle: 'Team',
+      ytitle: 'Average score'
+    }
+  end
+
+  def data
+    data = {}
+    Team.doubles.each do |team|
+      next unless team.rounds.count >= 5
+      scores = team.rounds.pluck(:score)
+      num_scores = scores.count
+      total_score = scores.sum
+      data[team.name] = (total_score.to_f / num_scores).round.to_i
+    end
+    return data.sort_by { |name, score| score }.reverse
+  end
+end
