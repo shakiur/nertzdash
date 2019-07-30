a3d9f5d3be43699314c8fcd09ef69100f9c81c14
Sam Yau <samyau21@gmail.com>
2019-07-30 16:29:02 -0400

    add backfill for archived teamgames

--- utilities/migration_backfills/AddArchivedToTeamGames.rb
+++ utilities/migration_backfills/AddArchivedToTeamGames.rb
@@ -1,1 +1,9 @@
-Game.unscoped.where(archived: true)
+Game.unscoped.where(archived: true).each do |game|
+  game.team_games.each do |team_game|
+    team_game.archived = true
+    team_game.save!
+  end
+  game.rounds.each do |round|
+    round.archived = true
+  end
+end
