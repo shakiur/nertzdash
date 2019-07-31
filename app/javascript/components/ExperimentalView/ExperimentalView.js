b87336af5f0b81a923db8b3f2534db2da08fb398
Kathy Wu <kathyw268@gmail.com>
2019-07-31 01:43:38 -0400

    add components and ability to select players

--- app/javascript/components/ExperimentalView/ExperimentalView.js
+++ app/javascript/components/ExperimentalView/ExperimentalView.js
@@ -1,12 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
+import SelectPlayers from "../SelectPlayers/SelectPlayers";
 
 class ExperimentalView extends React.Component {
+  state = {
+    showSelectPlayers: false
+  }
+
+  showSelectPlayers() {
+    this.setState({showSelectPlayers: true});
+  }
+
   render () {
     return (
       <div id="ExperimentalView">
-        <Button>Start Game</Button>
+        {!this.state.showSelectPlayers && <Button onClick={() => this.showSelectPlayers()}>Start Game</Button>}
+        {this.state.showSelectPlayers && <SelectPlayers/>}
       </div>
     );
   }
