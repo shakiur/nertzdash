01901bfe59abb56b4a77ed5fc0c44ac45c490735
Sam Yau <samyau21@gmail.com>
2019-07-31 16:59:07 -0400

    make team win percentage limit minimum of 10 rounds over 2 games

--- lib/stats/doubles/win_percentage_doubles.rb
+++ lib/stats/doubles/win_percentage_doubles.rb
@@ -6,7 +6,7 @@ module WinPercentageDoubles
   end
 
   def description
-    "Win percentage of teams that have won at least once. Only accounts for teams who have played at minimum of 8 rounds. Note that a Team that has only played once together and won can have a 100% win rate."
+    "Win percentage of teams per game. Only accounts for teams who have played at minimum of 10 rounds across 2 or more games."
   end
 
   def graph_type
@@ -27,7 +27,8 @@ module WinPercentageDoubles
     }.select { |team_game| team_game.doubles? }
 
     winning_team_games.map(&:team).uniq.each do |team|
-      next unless team.rounds.count >= 8
+      next unless team.rounds.count >= 10
+      next unless team.team_games.count >= 2
 
       number_of_times_won = winning_team_games.count { |team_game|
         team_game.team_id == team.id
