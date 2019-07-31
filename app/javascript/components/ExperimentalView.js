e994612852fb174032d1faf4c31971d044f77883
Kathy Wu <kathyw268@gmail.com>
2019-07-30 22:57:23 -0400

    add first component

---
+++ app/javascript/components/ExperimentalView.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ExperimentalView extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>klsjdfksdljfkl</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ExperimentalView
