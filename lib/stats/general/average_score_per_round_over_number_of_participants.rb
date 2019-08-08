ca0bb9325e176be164138e7fa074676171de593a
Sam Yau <samyau21@gmail.com>
2019-08-08 16:14:18 -0400

    finish avg score per round over num participants general stat

--- lib/stats/general/average_score_per_round_over_number_of_participants.rb
+++ lib/stats/general/average_score_per_round_over_number_of_participants.rb
@@ -6,23 +6,41 @@ module AverageScorePerRoundOverNumberOfParticipants
   end
 
   def description
-    "Average score per round based on the number of teams participating in a game."
+    "Average score per round based on the number of teams/players participating in a game."
   end
 
   def graph_type
-    :line_chart
+    :area_chart
   end
 
   def graph_options
     {
       xtitle: 'Number of participants',
-      ytitle: 'Average score per round'
+      ytitle: 'Average score per round',
+      discrete: true
     }
   end
 
   def data
-    data = {}
+    avg_scores = Hash.new { |h, k| h[k] = [] }
 
+    Round.select(
+      :game_id,
+      :round_number,
+      :score
+    ).group_by { |round|
+      [round.game_id, round.round_number]
+    }.each do |game_and_round_number, rounds|
+      num_participants = rounds.size
+      average_score_for_this_game_and_round = rounds.pluck(:score).sum.to_f / num_participants
+      avg_scores[num_participants] << average_score_for_this_game_and_round.round(2)
+    end
+
+    data = {}
+    avg_scores.each do |num_participants, average_scores|
+      avg_score_for_this_num_participants = average_scores.sum.to_f / average_scores.size
+      data[num_participants] = avg_score_for_this_num_participants.round(1)
+    end
 
     return data.sort_by { |num_participants, avg_score| avg_score }.reverse
   end
