5d987b08b9fbfba177e01ccf66799e45c03fbfa1
Shakiur Rahman <shakiur@gmail.com>
2021-01-17 16:34:40 -0500

    extract card into own component

---
+++ app/javascript/components/CardGameView/components/Card/Card.js
@@ -0,0 +1,13 @@
+import "./Card.scss"
+
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card">
+      </div>
+    )
+  }
+}
--- app/javascript/components/CardGameView/components/PlayerTable/PlayerTable.js
+++ app/javascript/components/CardGameView/components/PlayerTable/PlayerTable.js
@@ -3,6 +3,7 @@ import './PlayerTable.scss'
 import React from "react";
 import Draggable from "react-draggable";
 import PropTypes from "prop-types";
+import Card from "../../components/Card/Card"
 import CardHolder from "../../components/CardHolder/CardHolder"
 
 export default class PlayerTable extends React.Component {
@@ -11,11 +12,8 @@ export default class PlayerTable extends React.Component {
       <div className="PlayerTable">
         <div className="CardDeckArea">
           <Draggable>
-            <div className="Card">
-            </div>
+            <Card/>
           </Draggable>
-        </div>
-        <div className="CardDisplayArea">
         </div>
         <div className="SolitaireArea">
           <CardHolder/>
