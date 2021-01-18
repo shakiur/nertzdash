4d1bd9dd9c9c5ee4cbf95c154cdb19dad58dc8b2
Shakiur Rahman <shakiur@gmail.com>
2021-01-18 10:39:13 -0500

    change color of card while dragging

--- app/javascript/components/CardGameView/components/Card/Card.js
+++ app/javascript/components/CardGameView/components/Card/Card.js
@@ -5,10 +5,20 @@ import PropTypes from "prop-types";
 import Draggable from "react-draggable";
 
 export default class Card extends React.Component {
+  state = {
+    clickedCard: false
+  }
+
   render() {
+    const color = this.state.clickedCard ? `HighlightColor` : `DefaultColor`
+    const classes = `Card ${color}`
+
     return (
-      <Draggable>
-        <div className="Card">
+      <Draggable
+        onStart={() => this.setState({clickedCard: true})}
+        onStop={() => this.setState({clickedCard: false})}
+      >
+        <div className={classes}>
         </div>
       </Draggable>
     )
