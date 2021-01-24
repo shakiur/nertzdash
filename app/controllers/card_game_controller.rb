3914141761d1ce103cae76c6af2f178fda4f3b74
shakiur <shakiur@gmail.com>
2021-01-24 03:30:40 -0500

    implement controller to update card via broadcasting

--- app/controllers/card_game_controller.rb
+++ app/controllers/card_game_controller.rb
@@ -1,4 +1,12 @@
 class CardGameController < ApplicationController
   def index
   end
+
+  def update_position
+    ActionCable.server.broadcast 'card_game',
+      player_pos: params[:player_pos],
+      x_pos: params[:x_pos],
+      y_pos: params[:y_pos]
+    head :ok
+  end
 end
