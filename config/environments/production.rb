3717a97aa02f5ce38ab46ab57fbfce11a797926b
Shakiur Rahman <shakiur@gmail.com>
2021-01-25 10:26:00 -0500

    remove explicit urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.web_socket_server_url = "wss://nertzdash.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://nertzdash.herokuapp.com', 'http://nertzdash.herokuapp.com']
+  #config.web_socket_server_url = "wss://nertzdash.herokuapp.com/cable"
+  #config.action_cable.allowed_request_origins = ['https://nertzdash.herokuapp.com', 'http://nertzdash.herokuapp.com']
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
