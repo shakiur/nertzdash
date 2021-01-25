0c4974bf4de6db5faae1ee995c85676d446b2078
Shakiur Rahman <shakiur@gmail.com>
2021-01-25 10:32:59 -0500

    set action cable config url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,3 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
