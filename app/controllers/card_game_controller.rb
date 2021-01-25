25ea7cb31970b2117f7bd0cd69a47c96a437c32e
shakiur <shakiur@gmail.com>
2021-01-24 23:00:26 -0500

    add player_uuid and time to broadcast

--- app/controllers/card_game_controller.rb
+++ app/controllers/card_game_controller.rb
@@ -5,8 +5,10 @@ class CardGameController < ApplicationController
   def update_position
     ActionCable.server.broadcast 'card_game',
       player_pos: params[:player_pos],
+      player_uuid: params[:player_uuid],
       x_pos: params[:x_pos],
-      y_pos: params[:y_pos]
+      y_pos: params[:y_pos],
+      time: params[:time]
     head :ok
   end
 end
