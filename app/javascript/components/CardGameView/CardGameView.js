c779f064d38bab550b154cea12f62041c330f39d
shakiur <shakiur@gmail.com>
2021-01-29 22:35:54 -0500

    convert cardgame and player table to hooks

--- app/javascript/components/CardGameView/CardGameView.js
+++ app/javascript/components/CardGameView/CardGameView.js
@@ -1,32 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import PlayerTable from "./components/PlayerTable/PlayerTable"
 import CenterTable from "./components/CenterTable/CenterTable"
 
 const playerUuid = Math.random().toString(36).substring(7);
 
-class CardGameView extends React.Component {
-  render () {
-    return (
-      <section className="CardGameView">
-        <section className="TopRow">
-          <PlayerTable playerPos={1} playerUuid={playerUuid} />
-          <PlayerTable playerPos={2} playerUuid={playerUuid} />
-        </section>
+function CardGameView() {
+  const [player1XPos, setPlayer1XPos] = useState(0);
+  const [player1YPos, setPlayer1YPos] = useState(0);
 
-        <section className="MiddleRow">
-          <PlayerTable playerPos={3} playerUuid={playerUuid} />
-          <CenterTable />
-          <PlayerTable playerPos={4} playerUuid={playerUuid} />
-        </section>
+  const [player2XPos, setPlayer2XPos] = useState(0);
+  const [player2YPos, setPlayer2YPos] = useState(0);
 
-        <section className="BottomRow">
-          <PlayerTable playerPos={5} playerUuid={playerUuid} />
-          <PlayerTable playerPos={6} playerUuid={playerUuid} />
-        </section>
+  const [player3XPos, setPlayer3XPos] = useState(0);
+  const [player3YPos, setPlayer3YPos] = useState(0);
+
+  const [player4XPos, setPlayer4XPos] = useState(0);
+  const [player4YPos, setPlayer4YPos] = useState(0);
+
+  const [player5XPos, setPlayer5XPos] = useState(0);
+  const [player5YPos, setPlayer5YPos] = useState(0);
+
+  const [player6XPos, setPlayer6XPos] = useState(0);
+  const [player6YPos, setPlayer6YPos] = useState(0);
+
+  return (
+    <section className="CardGameView">
+      <section className="TopRow">
+        <PlayerTable
+          playerPos={1}
+          playerUuid={playerUuid}
+          xPos={player1XPos}
+          yPos={player1YPos}
+          setXPos={setPlayer1XPos}
+          setYPos={setPlayer1YPos}
+        />
+        <PlayerTable
+          playerPos={2}
+          playerUuid={playerUuid}
+          xPos={player2XPos}
+          yPos={player2YPos}
+          setXPos={setPlayer2XPos}
+          setYPos={setPlayer2YPos}
+        />
       </section>
-    );
-  }
+
+      <section className="MiddleRow">
+        <PlayerTable
+          playerPos={3}
+          playerUuid={playerUuid}
+          xPos={player3XPos}
+          yPos={player3YPos}
+          setXPos={setPlayer3XPos}
+          setYPos={setPlayer3YPos}
+        />
+        <CenterTable />
+        <PlayerTable
+          playerPos={4}
+          playerUuid={playerUuid}
+          xPos={player4XPos}
+          yPos={player4YPos}
+          setXPos={setPlayer4XPos}
+          setYPos={setPlayer4YPos}
+        />
+      </section>
+
+      <section className="BottomRow">
+        <PlayerTable
+          playerPos={5}
+          playerUuid={playerUuid}
+          xPos={player5XPos}
+          yPos={player5YPos}
+          setXPos={setPlayer5XPos}
+          setYPos={setPlayer5YPos}
+        />
+        <PlayerTable
+          playerPos={6}
+          playerUuid={playerUuid}
+          xPos={player6XPos}
+          yPos={player6YPos}
+          setXPos={setPlayer6XPos}
+          setYPos={setPlayer4YPos}
+        />
+      </section>
+    </section>
+  );
 }
 
 export default CardGameView
--- app/javascript/components/CardGameView/components/PlayerTable/PlayerTable.js
+++ app/javascript/components/CardGameView/components/PlayerTable/PlayerTable.js
@@ -3,36 +3,33 @@ import PropTypes from "prop-types";
 import Card from "../../components/Card/Card"
 import CardHolder from "../../components/CardHolder/CardHolder"
 
-export default class PlayerTable extends React.Component {
-  static propTypes = {
-    playerPos: PropTypes.number.isRequired,
-    playerUuid: PropTypes.string.isRequired
-  }
-
-  render() {
-    const { playerPos, playerUuid } = this.props;
-
-    return (
-      <div className="PlayerTable">
-        <div className="CardDeckArea">
-          <Card
-            playerPos={playerPos}
-            playerUuid={playerUuid}
-          />
-        </div>
-        <div className="SolitaireArea">
-          <CardHolder/>
-        </div>
-        <div className="SolitaireArea">
-          <CardHolder/>
-        </div>
-        <div className="SolitaireArea">
-          <CardHolder/>
-        </div>
-        <div className="SolitaireArea">
-          <CardHolder/>
-        </div>
+const PlayerTable = ({ playerPos, playerUuid, xPos, yPos, setXPos, setYPos}) => {
+  return (
+    <div className="PlayerTable">
+      <div className="CardDeckArea">
+        <Card
+          playerPos={playerPos}
+          playerUuid={playerUuid}
+          xPos={xPos}
+          yPos={yPos}
+          setXPos={setXPos}
+          setYPos={setYPos}
+        />
       </div>
-    )
-  }
+      <div className="SolitaireArea">
+        <CardHolder/>
+      </div>
+      <div className="SolitaireArea">
+        <CardHolder/>
+      </div>
+      <div className="SolitaireArea">
+        <CardHolder/>
+      </div>
+      <div className="SolitaireArea">
+        <CardHolder/>
+      </div>
+    </div>
+  )
 }
+
+export default PlayerTable;
