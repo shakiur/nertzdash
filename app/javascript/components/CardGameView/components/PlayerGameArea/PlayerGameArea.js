461c6cf8f9bbc9e92fe0e14f39e1bfe04f8c4f93
shakiur <shakiur@gmail.com>
2021-02-28 16:09:42 -0500

    implement dropdown and deal button

--- app/javascript/components/CardGameView/components/PlayerGameArea/PlayerGameArea.js
+++ app/javascript/components/CardGameView/components/PlayerGameArea/PlayerGameArea.js
@@ -60,8 +60,50 @@ const PlayerGameArea = ({
     }
   }
 
+  function PlayerGameControl() {
+    return (
+      <SelectPlayerDisplay />
+    )
+  }
+
+  function SelectPlayerDisplay() {
+    return (
+      <div className="SelectPlayerDisplay">
+        <select
+          value={playerName}
+          onChange={(event) => handlePlayerSelectChange(event)}
+          className="PlayerSelect"
+        >
+          <option key={0} value=''>Pick Player</option>
+          {
+            allPlayers.map((player) =>
+              <option key={playerPos+player.id} value={player.name}>
+                {player.name}
+              </option>
+            )
+          }
+        </select>
+        <button
+          onClick={() => handleDealCards()}
+          className="DealCardsButton"
+        >
+          Deal
+        </button>
+      </div>
+    )
+  }
+
+  function handlePlayerSelectChange(event) {
+    const selectedName = event.target.value
+    setPlayerName(selectedName)
+  }
+
+  function handleDealCards() {
+  }
+
   return (
     <div className="PlayerGameArea">
+      <PlayerGameControl />
     </div>
   )
 }
