6d72816c653b47fcd381e255866aeb77ea086ada
shakiur <shakiur@gmail.com>
2021-02-28 16:39:09 -0500

    remove old code

--- app/javascript/components/CardGameView/components/PlayerGameArea/PlayerGameArea.js
+++ app/javascript/components/CardGameView/components/PlayerGameArea/PlayerGameArea.js
@@ -29,37 +29,6 @@ const PlayerGameArea = ({
     setBroadcastPlayerUuid(playerUuid)
   }
 
-  function flipThreeSolitaireCards() {
-    moveSolitairePileToLeftoverPile()
-    resetSolitaireDeckIfEmpty()
-    flipThreeCardsFromSolitaireDeck()
-  }
-
-  function moveSolitairePileToLeftoverPile() {
-    if(solitairePile.length > 0) {
-      setSolitaireLeftoverPile(solitaireLeftoverPile => [...solitaireLeftoverPile, ...solitairePile.reverse()])
-      setSolitairePile([])
-    }
-  }
-
-  function resetSolitaireDeckIfEmpty() {
-    if(solitairePile.length == 0 && solitaireDeck.length == 0) {
-      setSolitaireDeck(solitaireLeftoverPile)
-      setSolitaireLeftoverPile([])
-    }
-  }
-
-  function flipThreeCardsFromSolitaireDeck() {
-    const numCardsToFlip = Math.min(solitaireDeck.length, 3)
-
-    for(let flipCount = 0; flipCount < numCardsToFlip; flipCount++) {
-      let cardFlipped = solitaireDeck.shift()
-
-      setSolitaireDeck(solitaireDeck.filter(card => cardFlipped['id'] !== card['id']))
-      setSolitairePile(solitairePile => [cardFlipped, ...solitairePile])
-    }
-  }
-
   function PlayerGameControl() {
     if(playerActive) {
       return (
